dc3fad3565c462cbbac828c9c090799cfeb721ea
Puja Sah <69917223+puja1999@users.noreply.github.com>
2021-03-06 19:17:15 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which browser can supports the transition property?",
+    answer: "All of the above",
+    options: [
+      "Internet Explorer 10",
+      "Firefox",
+      "Chrome",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following is NOT a Style?",
+    answer: "Orthogonal",
+    options: [
+      "Linked",
+      "Embedded",
+      "Inline",
+      "Orthogonal"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following is NOT a Hotspot tool?",
+    answer: "Orthogonal Hotspot Tool",
+    options: [
+      "Orthogonal Hotspot Tool",
+      "Rectangular Hotspot Tool",
+      "Oval Hotspot Tool",
+      "Polygon Hotspot Tool"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is not supported by older browsers?",
+    answer: "All of the above",
+    options: [
+      "CSS",
+      "Layers",
+      "Frames",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Twitter limits the number of characters per tweet is",
+    answer: "140",
+    options: [
+      "140",
+      "160",
+      "124",
+      "130"
+    ]
+  },
+  
+  ];
